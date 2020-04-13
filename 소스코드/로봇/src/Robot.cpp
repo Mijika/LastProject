83aa92c4a54410bc5d673b4dd629fb23929faa5f
Mijika <40686438+Mijika@users.noreply.github.com>
2020-04-13 18:46:21 +0900

    Add files via upload

--- 소스코드/로봇/src/Robot.cpp
+++ 소스코드/로봇/src/Robot.cpp
@@ -1,6 +1,6 @@
 #include "Robot.h"
 
-Robot::Sensor::begin(int rate) { //Robot 클래스 초기화
+void Robot::Sensor::begin(int rate) { //Robot 클래스 초기화
     Serial.println("센서들 초기화 중....");
     uart_gps.begin(BAUD_RATE);
     if(!bmp.begin(BMP085_ULTRAHIGHRES)) {
@@ -12,7 +12,7 @@ Robot::Sensor::begin(int rate) { //Robot 클래스 초기화
     Serial.println("센서들 초기화 완료....");
 }
 
-Robot::Sensor::void sening() { //sening 해서 내부 필드에 값 저장
+void Robot::Sensor::sening() { //sening 해서 내부 필드에 값 저장
     Serial.println("센싱중... ");
     delay(300);
     hardness = Sensing_gps_hardness();
@@ -78,18 +78,18 @@ String Robot::Sensor::Sensing_gps_altitud() {
     return String.valueOf(gps.f_altitude()); // gps 고도값을 string 형으로 형 변환 하여 리턴
 }
 String Robot::Sensor::Sensing_altitude_altitude() { //고도 센서에서 고도값 센싱
-    /*	고도 센서에서 고도값 가져오기
-    	//기상청에서 우리나라 해면 기압 확인해서 코드에 반영한다.
-    		-이유: 고도센서에서 설정한 디폴트 값이 우리 나라의 해면 기압이 아니라서 오차 있음
-    	return : (String)고도값
-    	// float 형의 값을 string 형으로 변환에서 리턴
+    /*  고도 센서에서 고도값 가져오기
+        //기상청에서 우리나라 해면 기압 확인해서 코드에 반영한다.
+            -이유: 고도센서에서 설정한 디폴트 값이 우리 나라의 해면 기압이 아니라서 오차 있음
+        return : (String)고도값
+        // float 형의 값을 string 형으로 변환에서 리턴
     */
     return String.valueOf(bmp.readAltitude(100560));
 }
 String Robot::Sensor::Sensing_altitude_temperature() { //고도 센서에서 온도값 센싱
-    /*	고도 센서에서 온도값 가져오기
-    	return : (String)온도값
-    	// float 형의 값을 string 형으로 변환에서 리턴
+    /*  고도 센서에서 온도값 가져오기
+        return : (String)온도값
+        // float 형의 값을 string 형으로 변환에서 리턴
     */
      return String.valueOf(bmp.readTemperature());
 }
@@ -108,4 +108,149 @@ String Robot::Sensor::get_altitude_altitude() { //고도 센서에서 고도값 
 }
 String Robot::Sensor::get_altitude_temperature() { //고도 센서에서 온도값 가져오기
     return temperature;
+}
+
+
+//--------------------------------Action--------------------//
+void Robot::Action::begin() {
+    //앞 바퀴 pwm 설정
+    ledcSetup(MOVE_FRONT_MOTOR_RIGHT_PWM_CH, 5000, 8);
+    ledcSetup(MOVE_FRONT_MOTOR_LEFT_PWM_CH, 5000, 8);
+    ledcSetup(DIAMETER_FRONT_MOTORPWM_CH, 5000, 8);
+    ledcAttachPin(MOVE_FRONT_MOTOR_RIGHT_PWM, MOVE_FRONT_MOTOR_RIGHT_PWM_CH);
+    ledcAttachPin(MOVE_FRONT_MOTOR_LEFT_PWM, MOVE_FRONT_MOTOR_LEFT_PWM_CH);
+    ledcAttachPin(DIAMETER_FRONT_MOTOR_PWM, DIAMETER_FRONT_MOTORPWM_CH);
+    //뒷 바퀴 pwm 설정
+    ledcSetup(MOVE_BACK_MOTOR_RIGHT_PWM_CH, 5000, 8);
+    ledcSetup(MOVE_BACK_MOTOR_LEFT_PWM_CH, 5000, 8);
+    ledcSetup(DIAMETER_BACK_MOTORPWM_CH, 5000, 8);
+    ledcAttachPin(MOVE_BACK_MOTOR_RIGHT_PWM, MOVE_BACK_MOTOR_RIGHT_PWM_CH);
+    ledcAttachPin(MOVE_BACK_MOTOR_LEFT_PWM, MOVE_BACK_MOTOR_LEFT_PWM_CH);
+    ledcAttachPin(DIAMETER_BACK_MOTOR_PWM, DIAMETER_BACK_MOTORPWM_CH);
+    //앞 바퀴 방향및 지름 모터 방향 설정
+    pinMode(MOVE_FRONT_MOTOR_RIGHT_EN1, OUTPUT);       // Motor RIGHT 방향설정1
+    pinMode(MOVE_FRONT_MOTOR_RIGHT_EN2, OUTPUT);       // Motor RIGHT 방향설정2
+    pinMode(MOVE_FRONT_MOTOR_LEFT_EN1, OUTPUT);       // Motor LEFR 방향설정1
+    pinMode(MOVE_FRONT_MOTOR_LEFT_EN2, OUTPUT);       // Motor LEFR 방향설정2
+    pinMode(DIAMETER_FRONT_MOTOR_EN1, OUTPUT);       // 지름 모터 방향설정1
+    pinMode(DIAMETER_FRONT_MOTOR_EN2, OUTPUT);       // 지름 모터 방향설정2
+    //뒤 바퀴 방향및 지름 모터 방향 설정
+    pinMode(MOVE_BACK_MOTOR_RIGHT_EN1, OUTPUT);       // Motor RIGHT 방향설정1
+    pinMode(MOVE_BACK_MOTOR_RIGHT_EN2, OUTPUT);       // Motor RIGHT 방향설정2
+    pinMode(MOVE_BACK_MOTOR_LEFT_EN1, OUTPUT);       // Motor LEFR 방향설정1
+    pinMode(MOVE_BACK_MOTOR_LEFT_EN2, OUTPUT);       // Motor LEFR 방향설정2
+    pinMode(DIAMETER_BACK_MOTOR_EN1, OUTPUT);       // 지름 모터 방향설정1
+    pinMode(DIAMETER_BACK_MOTOR_EN2, OUTPUT);       // 지름 모터 방향설정2
+}
+
+void Robot::Action::led_on() { //LED 전등 온
+    digitalWrite(LED_RIGHT, HIGH);
+    digitalWrite(LED_LEFT, HIGH);
+}
+void Robot::Action::led_off() { //LED 전등 오프
+    digitalWrite(LED_RIGHT, LOW);
+    digitalWrite(LED_LEFT, LOW);
+}
+
+void Robot::Action::move_forward() { //모터 전진
+    //앞 바퀴
+    digitalWrite(MOVE_FRONT_MOTOR_RIGHT_EN1, HIGH);
+    digitalWrite(MOVE_FRONT_MOTOR_RIGHT_EN2, LOW);
+    digitalWrite(MOVE_FRONT_MOTOR_LEFT_EN1, LOW);
+    digitalWrite(MOVE_FRONT_MOTOR_LEFT_EN2, HIGH);
+    ledcWrite(MOVE_FRONT_MOTOR_RIGHT_PWM_CH, MOVE_MOTOR_SPD);
+    ledcWrite(MOVE_FRONT_MOTOR_LEFT_PWM_CH, MOVE_MOTOR_SPD);
+    //뒷 바퀴
+    digitalWrite(MOVE_BACK_MOTOR_RIGHT_EN1, HIGH);
+    digitalWrite(MOVE_BACK_MOTOR_RIGHT_EN2, LOW);
+    digitalWrite(MOVE_BACK_MOTOR_LEFT_EN1, LOW);
+    digitalWrite(MOVE_BACK_MOTOR_LEFT_EN2, HIGH);
+    ledcWrite(MOVE_BACK_MOTOR_RIGHT_PWM_CH, MOVE_BACK_MOTOR_SPD);
+    ledcWrite(MOVE_BACK_MOTOR_LEFT_PWM_CH, MOVE_BACK_MOTOR_SPD);
+}
+void Robot::Action::move_reverse() { //모터 후진
+    //앞 바퀴
+    digitalWrite(MOVE_FRONT_MOTOR_RIGHT_EN1, LOW);
+    digitalWrite(MOVE_FRONT_MOTOR_RIGHT_EN2, HIGH);
+    digitalWrite(MOVE_FRONT_MOTOR_LEFT_EN1, HIGH);
+    digitalWrite(MOVE_FRONT_MOTOR_LEFT_EN2, LOW);
+    ledcWrite(MOVE_FRONT_MOTOR_RIGHT_PWM_CH, MOVE_MOTOR_SPD);
+    ledcWrite(MOVE_FRONT_MOTOR_LEFT_PWM_CH, MOVE_MOTOR_SPD);
+    //뒷 바퀴
+    digitalWrite(MOVE_BACK_MOTOR_RIGHT_EN1, LOW);
+    digitalWrite(MOVE_BACK_MOTOR_RIGHT_EN2, HIGH);
+    digitalWrite(MOVE_BACK_MOTOR_LEFT_EN1, HIGH);
+    digitalWrite(MOVE_BACK_MOTOR_LEFT_EN2, LOW);
+    ledcWrite(MOVE_BACK_MOTOR_RIGHT_PWM_CH, MOVE_BACK_MOTOR_SPD);
+    ledcWrite(MOVE_BACK_MOTOR_LEFT_PWM_CH, MOVE_BACK_MOTOR_SPD);
+}
+void Robot::Action::move_left_turn() { //모터 좌회전
+    //앞 바퀴
+    digitalWrite(MOVE_FRONT_MOTOR_RIGHT_EN1, HIGH);
+    digitalWrite(MOVE_FRONT_MOTOR_RIGHT_EN2, LOW);
+    digitalWrite(MOVE_FRONT_MOTOR_LEFT_EN1, HIGH);
+    digitalWrite(MOVE_FRONT_MOTOR_LEFT_EN2, LOW);
+    ledcWrite(MOVE_FRONT_MOTOR_RIGHT_PWM_CH, MOVE_MOTOR_SPD);
+    ledcWrite(MOVE_FRONT_MOTOR_LEFT_PWM_CH, MOVE_MOTOR_SPD);
+    //뒷 바퀴
+    digitalWrite(MOVE_BACK_MOTOR_RIGHT_EN1, HIGH);
+    digitalWrite(MOVE_BACK_MOTOR_RIGHT_EN2, LOW);
+    digitalWrite(MOVE_BACK_MOTOR_LEFT_EN1, HIGH);
+    digitalWrite(MOVE_BACK_MOTOR_LEFT_EN2, LOW);
+    ledcWrite(MOVE_BACK_MOTOR_RIGHT_PWM_CH, MOVE_BACK_MOTOR_SPD);
+    ledcWrite(MOVE_BACK_MOTOR_LEFT_PWM_CH, MOVE_BACK_MOTOR_SPD);
+}
+void Robot::Action::move_right_turn() { //모터 우회전
+    //앞 바퀴
+    digitalWrite(MOVE_FRONT_MOTOR_RIGHT_EN1, LOW);
+    digitalWrite(MOVE_FRONT_MOTOR_RIGHT_EN2, HIGH);
+    digitalWrite(MOVE_FRONT_MOTOR_LEFT_EN1, LOW);
+    digitalWrite(MOVE_FRONT_MOTOR_LEFT_EN2, HIGH);
+    ledcWrite(MOVE_FRONT_MOTOR_RIGHT_PWM_CH, MOVE_MOTOR_SPD);
+    ledcWrite(MOVE_FRONT_MOTOR_LEFT_PWM_CH, MOVE_MOTOR_SPD);
+    //뒷 바퀴
+    digitalWrite(MOVE_BACK_MOTOR_RIGHT_EN1, LOW);
+    digitalWrite(MOVE_BACK_MOTOR_RIGHT_EN2, HIGH);
+    digitalWrite(MOVE_BACK_MOTOR_LEFT_EN1, LOW);
+    digitalWrite(MOVE_BACK_MOTOR_LEFT_EN2, HIGH);
+    ledcWrite(MOVE_BACK_MOTOR_RIGHT_PWM_CH, MOVE_BACK_MOTOR_SPD);
+    ledcWrite(MOVE_BACK_MOTOR_LEFT_PWM_CH, MOVE_BACK_MOTOR_SPD);
+}
+void Robot::Action::move_stop() {
+    //앞 바퀴
+    digitalWrite(MOVE_FRONT_MOTOR_RIGHT_EN1, LOW);
+    digitalWrite(MOVE_FRONT_MOTOR_RIGHT_EN2, LOW);
+    digitalWrite(MOVE_FRONT_MOTOR_LEFT_EN1, LOW);
+    digitalWrite(MOVE_FRONT_MOTOR_LEFT_EN2, LOW);
+    ledcWrite(MOVE_FRONT_MOTOR_RIGHT_PWM_CH, 0);
+    ledcWrite(MOVE_FRONT_MOTOR_LEFT_PWM_CH, 0);
+    //뒷 바퀴
+    digitalWrite(MOVE_BACK_MOTOR_RIGHT_EN1, LOW);
+    digitalWrite(MOVE_BACK_MOTOR_RIGHT_EN2, LOW);
+    digitalWrite(MOVE_BACK_MOTOR_LEFT_EN1, LOW);
+    digitalWrite(MOVE_BACK_MOTOR_LEFT_EN2, LOW);
+    ledcWrite(MOVE_FRONT_MOTOR_RIGHT_PWM_CH, 0);
+    ledcWrite(MOVE_FRONT_MOTOR_LEFT_PWM_CH, 0);
+}
+
+//지름 모터
+void Robot::Action::front_diameter_up() { // 지름 증가
+    digitalWrite(DIAMETER_FRONT_MOTOR_EN1, HIGH);
+    digitalWrite(DIAMETER_FRONT_MOTOR_EN2, LOW);
+    ledcWrite(DIAMETER_FRONT_MOTORPWM_CH, MOVE_DIAMETER_MOTOR_SPD);
+}
+void Robot::Action::front_diameter_down() { // 지름 감소
+    digitalWrite(DIAMETER_FRONT_MOTOR_EN1, LOW);
+    digitalWrite(DIAMETER_FRONT_MOTOR_EN2, HIGH);
+    ledcWrite(DIAMETER_FRONT_MOTORPWM_CH, MOVE_DIAMETER_MOTOR_SPD);
+}
+void Robot::Action::back_diameter_up() { // 지름 증가
+    digitalWrite(DIAMETER_MOTOR_EN1, HIGH);
+    digitalWrite(DIAMETER_MOTOR_EN2, LOW);
+    ledcWrite(DIAMETER_BACK_MOTORPWM_CH, MOVE_DIAMETER_MOTOR_SPD);
+}
+void Robot::Action::back_diameter_down() { // 지름 감소
+    digitalWrite(DIAMETER_BACK_MOTOR_EN1, LOW);
+    digitalWrite(DIAMETER_BACK_MOTOR_EN2, HIGH);
+    ledcWrite(DIAMETER_BACK_MOTORPWM_CH, MOVE_DIAMETER_MOTOR_SPD);
 }
--- 소스코드/로봇/src/Robot.h
+++ 소스코드/로봇/src/Robot.h
@@ -1,5 +1,5 @@
 #ifndef _ROBOT_
-#defind _ROBOT_
+#define _ROBOT_
 
 #include "arduino.h"
 #include <SoftwareSerial.h>
@@ -12,20 +12,46 @@
 #define GPS_RX_PIN 6 //GPS RX 핀
 #define GPS_TX_PIN 5 //GPS TX 핀
 
-#define LED_RIGHT 5 //오른쪽 lED 전등의 제어 핀
-#define LED_LEFT 6 //오른쪽 lED 전등의 제어 핀
+#define LED_RIGHT 5 //오른쪽 전등의 제어 핀
+#define LED_LEFT 6 //왼쪽 전등의 제어 핀
 
-#define MOVE_MOTOR_RIGHT_EN1 10 //오른쪽 모터의 방향 지시 핀 1
-#define MOVE_MOTOR_RIGHT_EN2 11 //오른쪽 모터의 방향 지시 핀 2
-#define MOVE_MOTOR_RIGHT_PWM 12 //오른쪽 모터의 pwm 핀
+//--------------------------------앞 바퀴-----------------------------
+#define MOVE_FRONT_MOTOR_RIGHT_EN1 10 //앞쪽_오른쪽 모터의 방향 지시 핀 1
+#define MOVE_FRONT_MOTOR_RIGHT_EN2 11 //앞쪽_오른쪽 모터의 방향 지시 핀 2
+#define MOVE_FRONT_MOTOR_RIGHT_PWM 12 //앞쪽_오른쪽 모터의 pwm 핀
+#define MOVE_FRONT_MOTOR_RIGHT_PWM_CH 0 //앞쪽_오른쪽 모터의 pwm 채널
 
-#define MOVE_MOTOR_LEFT _EN1 13 //왼쪽 모터의 방향 지시 핀 1
-#define MOVE_MOTOR_LEFT _EN2 14 //왼쪽 모터의 방향 지시 핀 2
-#define MOVE_MOTOR_LEFT_PWM 15 //왼쪽 모터의 pwm 핀
+#define MOVE_FRONT_MOTOR_LEFT_EN1 13 //앞쪽_왼쪽 모터의 방향 지시 핀 1
+#define MOVE_FRONT_MOTOR_LEFT_EN2 14 //앞쪽_왼쪽 모터의 방향 지시 핀 2
+#define MOVE_FRONT_MOTOR_LEFT_PWM 15 //앞쪽_왼쪽 모터의 pwm 핀
+#define MOVE_FRONT_MOTOR_LEFT_PWM_CH 1 //앞쪽_왼쪽 모터의 pwm 채널
 
-#define DIAMETER_MOTOR_LEFT _EN1 13 //바퀴 지름 제어 모터의 방향 지시 핀 1
-#define DIAMETER_MOTOR_LEFT _EN2 14 //바퀴 지름 제어 모터의 방향 지시 핀 2
-#define DIAMETER_MOTOR_LEFT_PWM 15 //바퀴 지름 제어 모터의 pwm 핀
+#define DIAMETER_FRONT_MOTOR_EN1 13 //앞쪽_바퀴 지름 제어 모터의 방향 지시 핀 1
+#define DIAMETER_FRONT_MOTOR_EN2 14 //앞쪽_바퀴 지름 제어 모터의 방향 지시 핀 2
+#define DIAMETER_FRONT_MOTOR_PWM 15 //앞쪽_바퀴 지름 제어 모터의 pwm 핀
+#define DIAMETER_FRONT_MOTORPWM_CH 2 //앞쪽_지름 모터의 pwm 채널
+//--------------------------------앞 바퀴-----------------------------
+
+//--------------------------------뒤 바퀴-----------------------------
+#define MOVE_BACK_MOTOR_RIGHT_EN1 16 //뒤쪽_오른쪽 모터의 방향 지시 핀 1
+#define MOVE_BACK_MOTOR_RIGHT_EN2 17 //뒤쪽_오른쪽 모터의 방향 지시 핀 2
+#define MOVE_BACK_MOTOR_RIGHT_PWM 18 //뒤쪽_오른쪽 모터의 pwm 핀
+#define MOVE_BACK_MOTOR_RIGHT_PWM_CH 3 //뒤쪽_오른쪽 모터의 pwm 채널
+
+#define MOVE_BACK_MOTOR_LEFT_EN1 19 //뒤쪽_왼쪽 모터의 방향 지시 핀 1
+#define MOVE_BACK_MOTOR_LEFT_EN2 20 //뒤쪽_왼쪽 모터의 방향 지시 핀 2
+#define MOVE_BACK_MOTOR_LEFT_PWM 21 //뒤쪽_왼쪽 모터의 pwm 핀
+#define MOVE_BACK_MOTOR_LEFT_PWM_CH 4 //뒤쪽_왼쪽 모터의 pwm 채널
+
+#define DIAMETER_BACK_MOTOR_EN1 22 //뒤쪽_바퀴 지름 제어 모터의 방향 지시 핀 1
+#define DIAMETER_BACK_MOTOR_EN2 23 //뒤쪽_바퀴 지름 제어 모터의 방향 지시 핀 2
+#define DIAMETER_BACK_MOTOR_PWM 24 //뒤쪽_바퀴 지름 제어 모터의 pwm 핀
+#define DIAMETER_BACK_MOTORPWM_CH 5 //뒤쪽_지름 모터의 pwm 채널
+//--------------------------------뒤 바퀴-----------------------------
+
+#define MOVE_MOTOR_SPD 100 //이동 모터의 스피드
+#define MOVE_DIAMETER_MOTOR_SPD 10 //지름 모터의 딜레이
+
 
 #define MAIN_UART_RX 1 //ESP32-CMA 과 UART 통신에 사용되는 RX 핀
 #define MAIN_UART_TX 2 //ESP32-CMA 과 UART 통신에 사용되는 TX 핀
@@ -52,7 +78,7 @@ public :
         String Sensing_altitude_altitude();      //  고도 센서에서 고도값 센싱
         String Sensing_altitude_temperature();   //    고도 센서에서 온도값 센싱
     public :
-        begin(int rate); //Sensor 클래스 초기화
+        void begin(int rate); //Sensor 클래스 초기화
         void sening(); //sening 해서 내부 필드에 값 저장
 
         String get_gps_hardness();  //   gps 경도값 가져오기
@@ -64,7 +90,7 @@ public :
 
     class Action {
     public :
-        begin();
+        void begin();
         //LED 모듈에 불 들어 오게하는 코딩
         void led_on(); //LED 전등 온
         void led_off(); //LED 전등 오프
@@ -73,6 +99,12 @@ public :
         void move_reverse(); //모터 후진
         void move_left_turn(); //모터 좌회전
         void move_right_turn(); //모터 우회전
+        void move_stop(); //모터 정지
+        //지름 모터 2가지 모드 코딩(지름 커짐, 작아짐)
+        void front_diameter_up(); // 지름 증가
+        void front_diameter_down(); // 지름 감소
+        void back_diameter_up(); // 지름 증가
+        void back_diameter_down(); // 지름 감소
     };
 
     class Uart_Communication {
